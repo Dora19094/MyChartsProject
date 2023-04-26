4b990c1f42dc1a4d6966959a50b1ebcbb1f97981
ntua-el19950 <el19950@mail.ntua.gr>
2023-04-26 21:05:06 +0300

    Initial state of frontend. Created the first page. (2nd commit due to unversioned files).

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/Charts.js';
+import {Charts} from "./components/Charts";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/*<img src={logo} className="App-logo" alt="logo" />  /!* mycharts service logo*!/*/}
+
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<Charts />} />
+                  <Route path="/home" element={<Charts />} />
+              </Routes>
+          </BrowserRouter>
+      </header>
+    </div>
+  );
+}
+
+export default App;
