0fcc5a8f7fc749acfbfe047811499449ba3b92e6
ntua-el19950 <el19950@mail.ntua.gr>
2023-05-03 12:55:10 +0300

    Google sign in POST

--- frontend/src/components/Charts.js
+++ frontend/src/components/Charts.js
@@ -11,52 +11,24 @@ export function Charts() {
     const [show, setShow] = useState(false);
     function handleClose(){setShow(false);}
     function handleShow() {setShow(true);}
-    const [questionnaire, setQuestionnare] = useState();
-
-    function handleClick() {
-        const url = "http://localhost/auth/google";
-        const fetchData = async () => {
-            try {
-                const response = await fetch(url);
-                const data = await response.json();
-                setQuestionnare(data);
-            } catch (error) {
-                console.error(error);
+    const handleLogin = async googleData => {
+        const res = await fetch("/api/v1/auth/google", {
+            method: "POST",
+            body: JSON.stringify({
+                token: googleData.tokenId
+            }),
+            headers: {
+                "Content-Type": "application/json"
             }
-        };
-        fetchData();
-    }
-    console.log(questionnaire);
-    function responseMessage(response){
-
-        console.log(response);
-
-            // Send user data to backend API
-            // fetch('/api/user', {
-            //     method: 'POST',
-            //     headers: {
-            //         'Content-Type': 'application/json',
-            //     },
-            //     body: JSON.stringify(response),
-            // })
-            //     .then(response => response.json())
-            //     .then(data => console.log(data))
-            //     .catch(error => console.error(error));
-
-    }
-    function errorMessage(error) {
-        console.log(error);
+        })
+        const data = await res.json()
+        // store returned user somehow
     }
 
     return (
-
             <div>
                 <img src={logo} className="App-logo" alt="logo" />
             <div>
-
-                <Button variant="primary" onClick ={handleClick()}>
-                </Button>
-
 <div>
     <Button variant="outline-info" onClick={handleShow}>
         Log in or Sign in with Google
@@ -68,7 +40,13 @@ export function Charts() {
         </Modal.Header>
         <Modal.Body> Sign in with your Google Account
 
-            <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+            <GoogleLogin
+                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                buttonText="Log in with Google"
+                onSuccess={handleLogin}
+                onFailure={handleLogin}
+                cookiePolicy={'single_host_origin'}
+            />
 
         </Modal.Body>
         <Modal.Footer>
