a1295e6ecf90ea18f9791e8beb38b79656b9b753
Theodora Boutsini <115620481+ntua-el19094@users.noreply.github.com>
2023-05-23 21:39:17 +0300

    count charts and corrections in user chart ms

--- microservices/user charts/src/controllers/chartFetchController.js
+++ microservices/user charts/src/controllers/chartFetchController.js
@@ -2,8 +2,7 @@ const fetchChart = require('../services/fetchChart');
 const chartFetchController = async (req, res) => {
 
     try {
-        const chartName = req.params.chartName; // JSON data sent from the frontend
-        const result = await fetchChart(chartName/*,userId*/);
+        const result = await fetchChart();
         // Send a response back to the frontend
         res.send(result);
     } catch (error) {
