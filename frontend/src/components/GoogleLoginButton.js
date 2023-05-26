6e72e5c8d42fb832eb4fbfffd240e267b2535683
ntua-el19950 <el19950@mail.ntua.gr>
2023-05-26 19:26:06 +0300

    google log in new user

--- frontend/src/components/GoogleLoginButton.js
+++ frontend/src/components/GoogleLoginButton.js
@@ -7,8 +7,10 @@ const GoogleLoginButton = () => {
 
     const success = (response) => {
 
-        console.log('Login success. Response:', response);
-        fetch('http://localhost:4000/auth/login', {
+
+        //--------------------------------------------
+
+        fetch('http://localhost:4000/auth/checkIfNewUser', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -22,15 +24,75 @@ const GoogleLoginButton = () => {
             .catch(error => {
                 console.error(error);
             });
-        navigate(
-            `/account/${response.credential}`,
-            {
-                // state: {
-                //     accessToken: data.accessToken,
-                //     refreshToken: data.refreshToken,
-                // },
 
-            });
+
+        if (response.newUser === false) {
+
+            //console.log('Login success. Response:', response);
+            fetch('http://localhost:4000/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(response1)
+            })
+                .then(response1 => response1.text())
+                .then(data1 => {
+                    console.log(data1);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+            navigate(
+                `/account/${response1.credential}`,
+                {
+                    // state: {
+                    //     accessToken: data.accessToken,
+                    //     refreshToken: data.refreshToken,
+                    // },
+                    // var accessToken = gapi.auth. getToken () .access_ token;
+
+                });
+        } else {
+            navigate(
+                `/account/${response.newUser}/false`,
+                {
+                    // state: {
+                    //     accessToken: data.accessToken,
+                    //     refreshToken: data.refreshToken,
+                    // },
+                    // var accessToken = gapi.auth. getToken () .access_ token;
+
+                });
+        }
+        //--------------------------------------------
+
+
+        //     console.log('Login success. Response:', response);
+        //     fetch('http://localhost:4000/auth/login', {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json'
+        //         },
+        //         body: JSON.stringify(response)
+        //     })
+        //         .then(response => response.text())
+        //         .then(data => {
+        //             console.log(data);
+        //         })
+        //         .catch(error => {
+        //             console.error(error);
+        //         });
+        //     navigate(
+        //         `/account/${response.credential}`,
+        //         {
+        //             // state: {
+        //             //     accessToken: data.accessToken,
+        //             //     refreshToken: data.refreshToken,
+        //             // },
+        //             // var accessToken = gapi.auth. getToken () .access_ token;
+        //
+        //         });
     };
 
 
