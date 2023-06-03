82800bcc5aef9d968c734b6c5345a3c1bb34ba97
ntua-el19950 <el19950@mail.ntua.gr>
2023-06-03 21:11:36 +0300

    correction buy credits

--- frontend/src/components/BuyCredits.js
+++ frontend/src/components/BuyCredits.js
@@ -10,7 +10,7 @@ import "./GoogleLoginButton.js"
 export default function BuyCredits() {
     const navigate = useNavigate();
     const {credentials} = useParams();
-    const [credits, setCredits] = useState([]);
+    const [credits, setCredits] = useState();
     // const five = [5], ten = [10], fifteen = [15], twenty = [20];
     const {state} = useLocation();
 
@@ -21,9 +21,7 @@ export default function BuyCredits() {
 
     function handle10() {
         console.log("bought 10 credits!");
-        setCredits({
-            count: 10
-        });
+        setCredits([10]);
     }
 
     function handle15() {
@@ -81,7 +79,7 @@ export default function BuyCredits() {
             //         );
             //     console.log(credits);
             //
-            fetch(`http://localhost:6000/credits/purchaseCredits/${credits}`, {
+            fetch(`http://localhost:6000/credits/purchaseCredits/${credits[5]}`, {
                 method: 'POST',
                 headers: {
                     'Authorization': `Bearer ${state.accessToken}`,
